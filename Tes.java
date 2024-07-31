860c59cbc0e0bc12821be44f7c2b7fcbd71f43a2
RASYID225 <rasyid_purwanto_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:16:19 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+    public static void main(String[] args) {
+     String nama = "Rasyid";
+     String kelas = "X RPL 5";
+     System.Out.print("Halo, nama saya" + nama + " Dari kelas" + kelas);   
+    }
+}
